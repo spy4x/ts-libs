0b541c65ee917c430f811735941f033cbcbf5da9
Anton Shubin <2spy4x@gmail.com>
2026-09-21 01:25:52 +0700

    refactor(platform): move MIN_PASSWORD_LENGTH to universal/constants
    It is not a time unit; time-constants.ts is derived millisecond and
    second constants, and this doesn't belong beside them.

--- platform/universal/constants.ts
+++ platform/universal/constants.ts
@@ -6,3 +6,6 @@ export const DEFAULT_CONCURRENCY = 4
 
 /** Default window for the throttled JSON saver, in milliseconds. */
 export const DEFAULT_FLUSH_INTERVAL_MS = 2000
+
+/** Minimum accepted password length. */
+export const MIN_PASSWORD_LENGTH = 12
